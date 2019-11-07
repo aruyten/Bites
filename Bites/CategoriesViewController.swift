bcae667d5437688a916d12a3278b0be25434e273
Adam <aruyten@gmail.com>
2019-11-06 21:00:14 -0500

    added vc files

---
+++ Bites/CategoriesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoriesViewController.swift
+//  Bites
+//
+//  Created by Adam Ruyten on 11/6/19.
+//  Copyright © 2019 Adam Ruyten. All rights reserved.
+//
+import UIKit
+
+
+class CategoriesViewController: UIViewController{
+	
+	//category button outlets
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	override func viewDidLoad() {
+		//perform basic loading here
+	}
+}
---
+++ Bites/FavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewController.swift
+//  Bites
+//
+//  Created by Adam Ruyten on 11/6/19.
+//  Copyright © 2019 Adam Ruyten. All rights reserved.
+//
+
+import Foundation
