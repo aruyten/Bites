7fd1dd64bbbb871384b27b67cad1337d10b67868
Adam <aruyten@gmail.com>
2019-11-06 20:33:08 -0500

    Initial Commit

---
+++ Bites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bites
+//
+//  Created by Adam Ruyten on 11/6/19.
+//  Copyright © 2019 Adam Ruyten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
