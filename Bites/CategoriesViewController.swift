a375db4d551d4a5116b462136a7a2880df66b104
Adam <aruyten@gmail.com>
2019-11-07 22:00:56 -0500

    result table is displaying sorted results in meters

--- Bites/CategoriesViewController.swift
+++ Bites/CategoriesViewController.swift
@@ -12,11 +12,11 @@ import MapKit
 class CategoriesViewController: UIViewController{
 	
 	//list of restaurants from search
-	var MexicanList: [String] = []
-	var PizzaList: [String] = []
-	var ChineseList: [String] = []
+	var MexicanList: [(String, CLLocationDistance)] = []
+	var PizzaList: [(String, CLLocationDistance)]  = []
+	var ChineseList: [(String, CLLocationDistance)]  = []
 	
-	var sendingList: [String] = []
+	var sendingList: [(String, CLLocationDistance)]  = []
 	
 	//create location manager
 	let locationManager = CLLocationManager()
@@ -26,27 +26,20 @@ class CategoriesViewController: UIViewController{
 	@IBOutlet weak var PizzaButton: UIButton!
 	@IBOutlet weak var ChineseButton: UIButton!
 	
-	
-	
-	
-	
 	@IBAction func PressMexicanButton(_ sender: UIButton) {
+		MexicanList.sort{if ($0.1 != $1.1){return ($0.1 < $1.1)} else{return $0.0 < $1.0}}
 		sendingList = MexicanList
 	}
 	
 	@IBAction func PressPizzaButton(_ sender: Any) {
+		PizzaList.sort{if ($0.1 != $1.1){return ($0.1 < $1.1)} else{return $0.0 < $1.0}}
 		sendingList = PizzaList
 	}
 	
 	@IBAction func PressChineseButton(_ sender: UIButton) {
+		ChineseList.sort{if ($0.1 != $1.1){return ($0.1 < $1.1)} else{return $0.0 < $1.0}}
 		sendingList = ChineseList
 	}
-	
-	
-	
-	
-	
-	
 	
 	//only transitions to results if location services are permitted
 	override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
@@ -70,10 +63,6 @@ class CategoriesViewController: UIViewController{
 		}
 	}
 	
-	
-	
-	
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		locationManager.delegate = self
@@ -85,9 +74,12 @@ class CategoriesViewController: UIViewController{
 			locationManager.requestWhenInUseAuthorization()
 		}
 	}
+
 	
 	
 	
+	
+//food search functions
 	func SearchForMexicanFood(){
 		let request = MKLocalSearch.Request()
 		
@@ -97,8 +89,13 @@ class CategoriesViewController: UIViewController{
 		
 		let search = MKLocalSearch(request: request)
 		search.start(completionHandler: {(response, error) in for item in response!.mapItems {
-			self.MexicanList.append(item.name!)}})
-	}
+			let location = CLLocation(latitude: item.placemark.coordinate.latitude, longitude: item.placemark.coordinate.longitude)
+			let distance = self.locationManager.location!.distance(from: location)
+			self.MexicanList.append((item.name!, distance))}})
+
+
+}
+
 	
 	func SearchForPizzaFood(){
 		let request = MKLocalSearch.Request()
@@ -109,7 +106,9 @@ class CategoriesViewController: UIViewController{
 		
 		let search = MKLocalSearch(request: request)
 		search.start(completionHandler: {(response, error) in for item in response!.mapItems {
-			self.PizzaList.append(item.name!)}})
+			let location = CLLocation(latitude: item.placemark.coordinate.latitude, longitude: item.placemark.coordinate.longitude)
+			let distance = self.locationManager.location!.distance(from: location)
+			self.PizzaList.append((item.name!, distance))}})
 	}
 	
 	func SearchForChineseFood(){
@@ -121,10 +120,15 @@ class CategoriesViewController: UIViewController{
 		
 		let search = MKLocalSearch(request: request)
 		search.start(completionHandler: {(response, error) in for item in response!.mapItems {
-			self.ChineseList.append(item.name!)}})
+			let location = CLLocation(latitude: item.placemark.coordinate.latitude, longitude: item.placemark.coordinate.longitude)
+			let distance = self.locationManager.location!.distance(from: location)
+			self.ChineseList.append((item.name!, distance))}})
+		
+		
 	}
-	
 }
+
+
 extension CategoriesViewController: CLLocationManagerDelegate{
 	
 	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
--- Bites/ResultsViewController.swift
+++ Bites/ResultsViewController.swift
@@ -12,11 +12,13 @@ import MapKit
 
 class ResultsViewController: UITableViewController{
 
-	var sentList: [String] = []
+	var sentList: [(String, CLLocationDistance)] = []
 	
 
 	
 	@IBOutlet var ResultsTable: UITableView!
+	@IBOutlet weak var DistanceCell: UILabel!
+	@IBOutlet weak var NameCell: UILabel!
 	
 	override func viewDidLoad() {
 
@@ -29,14 +31,18 @@ class ResultsViewController: UITableViewController{
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		
 		let cell = tableView.dequeueReusableCell(withIdentifier: "StuffCell", for: indexPath)
-		cell.textLabel?.text = "\(sentList[indexPath.item])"
+		cell.textLabel?.text = sentList[indexPath.item].0
+		//String(format: "%.1f", convertedValue)
+		cell.detailTextLabel?.text = String(format: "%.2f", sentList[indexPath.item].1) +  " Meters"
 		return cell
 	}
 
 	override func numberOfSections(in tableView: UITableView) -> Int {
 		return 1
 	}
+}
 
-
+class StuffCell: UITableViewCell{
+	@IBOutlet weak var DistanceCell: UILabel!
 
 }
